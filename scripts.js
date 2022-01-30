4b7b243d13412940d5538490241d7a1c0c9f11f7
NikoLull <nikolai.haugland@outlook.com>
2022-01-30 18:13:18 +0100

    updated roc paper scissors game

--- scripts.js
+++ scripts.js
@@ -1,36 +1,42 @@
+let playerScore = 0
+let computerScore = 0
 
+    
 
+        function computerPlay(){
+            let play =[
+                        "rock",
+                        "paper",
+                        "scissors"
+            ]
+            return play[Math.floor(Math.random()*play.length)]
+    }
 
-function computerPlay(){
-     var play= [
-         "rock",
-         "paper",
-         "scissor"
-    ]
-     
-       
-   return play[Math.floor(Math.random()*play.length)];
-   
-  
-}
-console.log(computerPlay())
+    
 
-const playerSelection = "rock";
-const computerSelection =computerPlay() 
-
-
-function playRound(playerSelection,computerSelection){
-
-
-if (playerSelection > computerSelection){
-    return "huray"
-} else {
-    return "naay"
-}
- 
-
-}
-
-console.log(playRound())
-
-
+     computerSelection=computerPlay();
+    playerSelection = window.prompt("Rock, Paper or Scissors?");
+    
+    function playRound(){
+    if ((playerSelection === "rock" && computerSelection === "scissors") || 
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")){
+            playerScore += 1;
+            console.log(`you win! ${playerSelection}  beats  ${computerSelection}`)
+            if (playerScore === 5){
+                return console.log("You Won The Game!")
+            }
+        } else if (playerSelection == computerSelection ) {
+            playerScore += 1
+            computerScore += 1
+            console.log(`it's a tie, you both chose ${playerSelection}`)
+            
+        }
+        else {
+            computerScore += 1;
+            console.log(`You lose ${computerSelection} beats ${playerSelection}`)
+        } if (computerScore == 5){
+            return console.log("You Lost The Game")
+        }   return playRound()
+    }
+    console.log(playRound())
